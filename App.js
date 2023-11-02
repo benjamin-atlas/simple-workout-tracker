2e4ba3bd2a14da42590955e93c7e0d9e92bf4b00
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2023-11-01 23:20:48 -0600

    Basic firebase setup

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import * as eva from "@eva-design/eva";
 import {
   ApplicationProvider,
   IconRegistry,
   Layout,
-  Text
+  Text,
 } from "@ui-kitten/components";
 import { default as theme } from "./assets/theme/custom-theme.json";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import { List, ListItem } from "@ui-kitten/components";
-
-const data = new Array(8).fill({
-  title: "Phase",
-});
+import { initializeApp } from "firebase/app";
+import {
+  signInWithEmailAndPassword,
+  initializeAuth,
+} from "firebase/auth";
+import { getReactNativePersistence, ReactNativeAsyncStorage } from "firebase/auth";
+import { getDatabase, ref, onValue, set, push, child } from "firebase/database";
 
 const styles = StyleSheet.create({
   container: {
@@ -30,11 +33,52 @@ const styles = StyleSheet.create({
 });
 
 export default () => {
+  const firebaseConfig = {
+    apiKey: process.env.EXPO_PUBLIC_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+    databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL,
+    projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID
+  };
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+    const auth = initializeAuth(app, {
+      persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+    });
+
+    signInWithEmailAndPassword(
+      auth,
+      process.env.EXPO_PUBLIC_EMAIL,
+      process.env.EXPO_PUBLIC_PASSWORD
+    )
+      .then(() => {
+        onValue(
+          ref(database, `program`),
+          (snapshot) => {
+            setData(snapshot.val());
+          },
+          (err) => {
+            console.log(`An error occured fetching the program: ${err}`);
+          }
+        );
+      })
+      .catch((err) => {
+        console.log(`An error occured signing in: ${err}`);
+      });
+  }, []);
+
   const renderItem = ({ item, index }) => (
     <ListItem
       title={() => (
         <Text category="p1">
-          {item.title} {index + 1}
+          Phase {index + 1}
         </Text>
       )}
     />
