313d3dd483264f7e6027589fc1b5911fa45776cc
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2023-11-09 00:54:05 -0700

    Moving keyboard up on input focus

--- exercise-manager.component.js
+++ exercise-manager.component.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { View, SafeAreaView } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  SafeAreaView,
+} from "react-native";
 import {
   Button,
   Card,
@@ -34,70 +39,75 @@ export const ExerciseManagerScreen = ({ route }) => {
   return (
     <Layout style={{ flex: 1 }}>
       <SafeAreaView style={{ flex: 1 }}>
-        <ViewPager
+        <KeyboardAvoidingView
           style={{ flex: 1 }}
-          selectedIndex={selectedIndex}
-          onSelect={setSelectedIndex}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
         >
-          {exercises.map((exercise, index) => {
-            const loadInputState = useInputState(exercise.load.toString());
-            const lsrpeInputState = useInputState(exercise.lsrpe.toString());
+          <ViewPager
+            style={{ flex: 1 }}
+            selectedIndex={selectedIndex}
+            onSelect={setSelectedIndex}
+          >
+            {exercises.map((exercise, index) => {
+              const loadInputState = useInputState(exercise.load.toString());
+              const lsrpeInputState = useInputState(exercise.lsrpe.toString());
 
-            return (
-              <Layout
-                style={{
-                  flex: 1,
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-                key={index + 1}
-              >
-                <Card>
-                  <View style={{ flexDirection: "column", rowGap: "20px" }}>
-                    <Text category="h3">{exercise.workoutTitle}</Text>
-                    <Text category="p1">{exercise.notes}</Text>
-                    <Text category="h5">Sets: {exercise.workingSets}</Text>
-                    <Text category="h5">Reps: {exercise.reps}</Text>
-                    <Text category="h5">Rest: {exercise.rest}</Text>
-                    <Text category="h5">Target RPE: {exercise.rpe}</Text>
-                    <Input label="Load" {...loadInputState}></Input>
-                    <Input label="LSRPE" {...lsrpeInputState}></Input>
-                    <Button
-                      style={{ justifyContent: "flex-start" }}
-                      size="large"
-                      accessoryRight={(props) =>
-                        exerciseSaving === "pending" ? (
-                          <View style={[props.style]}>
-                            <Spinner status="info" />
-                          </View>
-                        ) : (
-                          <View style={[props.style]}></View>
-                        )
-                      }
-                      onPress={() => {
-                        dispatch(
-                          updateExerciseAsync({
-                            phaseIndex: route.params.phase,
-                            weekIndex: route.params.week,
-                            dayIndex: route.params.day,
-                            exerciseIndex: index,
-                            newExerciseValues: {
-                              ...exercise,
-                              load: loadInputState.value,
-                              lsrpe: lsrpeInputState.value,
-                            },
-                          })
-                        );
-                      }}
-                    >
-                      Save Changes
-                    </Button>
-                  </View>
-                </Card>
-              </Layout>
-            );
-          })}
-        </ViewPager>
+              return (
+                <Layout
+                  style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                  key={index + 1}
+                >
+                  <Card>
+                    <View style={{ flexDirection: "column", rowGap: "20px" }}>
+                      <Text category="h3">{exercise.workoutTitle}</Text>
+                      <Text category="p1">{exercise.notes}</Text>
+                      <Text category="h5">Sets: {exercise.workingSets}</Text>
+                      <Text category="h5">Reps: {exercise.reps}</Text>
+                      <Text category="h5">Rest: {exercise.rest}</Text>
+                      <Text category="h5">Target RPE: {exercise.rpe}</Text>
+                      <Input label="Load" {...loadInputState}></Input>
+                      <Input label="LSRPE" {...lsrpeInputState}></Input>
+                      <Button
+                        style={{ justifyContent: "flex-start" }}
+                        size="large"
+                        accessoryRight={(props) =>
+                          exerciseSaving === "pending" ? (
+                            <View style={[props.style]}>
+                              <Spinner status="info" />
+                            </View>
+                          ) : (
+                            <View style={[props.style]}></View>
+                          )
+                        }
+                        onPress={() => {
+                          dispatch(
+                            updateExerciseAsync({
+                              phaseIndex: route.params.phase,
+                              weekIndex: route.params.week,
+                              dayIndex: route.params.day,
+                              exerciseIndex: index,
+                              newExerciseValues: {
+                                ...exercise,
+                                load: loadInputState.value,
+                                lsrpe: lsrpeInputState.value,
+                              },
+                            })
+                          );
+                        }}
+                      >
+                        Save Changes
+                      </Button>
+                    </View>
+                  </Card>
+                </Layout>
+              );
+            })}
+          </ViewPager>
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </Layout>
   );
